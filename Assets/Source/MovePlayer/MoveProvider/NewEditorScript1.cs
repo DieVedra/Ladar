37ba2327b3c17181be20a95209be0feee667af06
DieVedra <71323552+DieVedra@users.noreply.github.com>
2022-09-29 23:24:54 +0300

    anon
    test

---
+++ Assets/Source/MovePlayer/MoveProvider/NewEditorScript1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Source.MovePlayer.MoveProvider
+{
+    public class NewEditorScript1 : ScriptableObject
+    {
+        [MenuItem("Tools/MyTool/Do It in C#")]
+        static void DoIt()
+        {
+            EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+        }
+    }
+}
